561ea698d61a57f54307fd1b4dafc4fdea06fb7f
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-08-16 19:22:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,11 +10,7 @@ input.onButtonPressed(Button.A, function () {
     ship.turn(Direction.Left, 45)
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 4; index++) {
-        ship.move(1)
-        ship.ifOnEdgeBounce()
-        basic.pause(100)
-    }
+    speed += 2
     game.setScore(scor)
 })
 input.onButtonPressed(Button.B, function () {
@@ -29,6 +25,7 @@ let scor = 0
 let asteroid: game.LedSprite = null
 let ast3: game.LedSprite = null
 let ship: game.LedSprite = null
+let speed = 0
 images.createBigImage(`
     . . . # # # . # . .
     . . # # . . . . . .
@@ -49,6 +46,16 @@ mkAst2()
 mkAst3()
 game.setLife(3)
 ship = game.createSprite(0, 0)
+basic.forever(function () {
+    for (let index = 0; index < speed; index++) {
+        ship.move(1)
+        ship.ifOnEdgeBounce()
+    }
+    speed += -1
+    if (speed < 0) {
+        speed = 0
+    }
+})
 basic.forever(function () {
     basic.pause(300)
     asteroid.move(1)
